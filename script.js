3257bd04936fa20f41be173a02a6d15febbd732c
MaciejOstojak <04macciej@gmail.com>
2018-12-06 19:50:04 +0100

    up

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const buttonsKey = document.getElementsByClassName('key');
+let lastKey;
+
 function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
+function getRandomKey() {
+    randomKey = Math.floor(math.random() * buttonsKey.lenght);
+
+    const key = buttonsKey[index];
+    if (key === lastKey) {
+        console.log('The same key!!!')
+        return getRandomKey()  
+    }
+    lastKey = key;
+    return key;
+};
+console.log(getRandomKey());
+
+// do domu napisz funkcję która losowo dodaje do klawisza klasę fire-up 
